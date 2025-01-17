cdb9ec9017b4d46135be74ffe74a0f6d5e4e6c44
辉夜 <rx700@vip.qq.com>
2025-01-17 15:27:40 +0800

    Add files via upload

--- CommonUpdater/UpdaterHelper/ProjectInfo.cs
+++ CommonUpdater/UpdaterHelper/ProjectInfo.cs
@@ -7,7 +7,7 @@ namespace UpdaterHelper
         public string ProjectName { get; set; } = String.Empty;
         public string ProjectExeName { get; set; } = String.Empty;
         public string ProjectAuthor { get; set; } = String.Empty;
-        public string ProjectBranch { get; set; } = String.Empty;
+        // public string ProjectBranch { get; set; } = String.Empty;
         public string ProjectCurrentVersion { get; set; } = String.Empty;
         public string ProjectCurrentExePath { get; set; } = String.Empty;
         public string ProjectNewExePath { get; set; } = String.Empty;
