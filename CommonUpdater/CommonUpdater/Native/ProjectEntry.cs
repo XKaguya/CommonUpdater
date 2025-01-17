4ba2d1bfc3575025ce2c27209b76c532da86001f
辉夜 <rx700@vip.qq.com>
2025-01-17 15:27:20 +0800

    Add files via upload

---
+++ CommonUpdater/CommonUpdater/Native/ProjectEntry.cs
@@ -0,0 +1,10 @@
+﻿namespace CommonUpdater.Native;
+
+public class ProjectEntry
+{
+    public bool MaintenanceMode { get; set; } = false;
+    public string ProjectName { get; set; } = string.Empty;
+    public string ProjectCurrentVersion { get; set; } = string.Empty;
+    public string ProjectVersion { get; set; } = string.Empty;
+    public bool ProjectForceUpdate { get; set; } = false;
+}
---
+++ CommonUpdater/CommonUpdater/Native/StatusCode.cs
@@ -0,0 +1,9 @@
+﻿namespace CommonUpdater.Native;
+
+public enum StatusCode
+{
+    Update = 0,
+    NoUpdate = 1,
+    ForceUpdate = 2,
+    Null = 255,
+}
